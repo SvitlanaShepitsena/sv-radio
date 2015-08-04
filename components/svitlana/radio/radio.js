36c2f681ac4470d1a463da973ea08c260a566543
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-08-04 12:36:06 -0500

    Message

--- components/svitlana/radio/radio.js
+++ components/svitlana/radio/radio.js
@@ -1,27 +1,23 @@
 FamousFramework.scene('svitlana:radio', {
     behaviors: {
-        '#background': {
-            'style': {
-                'background-color': 'lightgrey'
-            }
-        },
         '#radioRoot': {
             'align': [0.5, 0.5],
             'origin': [0.5, 0.5],
             'mount-point': [0.5, 0.5],
             'size': [250, 250],
-            //'content': function () {
-            //    return ``;
-            //},
+            'style': {
+                'background-color': '#F5F5F5',
+                'perspective': '10000px'
+            },
+        },
+        '#frontFace': {
+            'align': [0.5, 0.5],
+            'origin': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'size': [250, 250],
             'rotation-y': function (angle) {
-                // console.log(angle);
                 return angle;
             },
-            'style': {
-                'border-radius': '2px',
-                'background-color': '#F5F5F5'
-            },
-            'unselectable': true
         },
         '#radioTitle': {
             'content': function () {
@@ -32,7 +28,6 @@ FamousFramework.scene('svitlana:radio', {
                 'text-align': 'center',
                 'color': '#616161'
             },
-            'unselectable': true
         },
         '#radioLogo': {
             'align': [0.5, 0.6],
@@ -43,10 +38,62 @@ FamousFramework.scene('svitlana:radio', {
                 return `<img src='https://s3-us-west-2.amazonaws.com/svet.com/radio/wasp.jpg'/>`;
             },
             'style': {
-                'border-radius': '2px',
-                'background-color': '#F5F5F5'
+                'border-radius': '2px'
+
             },
-            'unselectable': true
+        },
+        '#programsRoot': {
+            'align': [0.5, 0.6],
+            'origin': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'size': [200, 200],
+            'style': {
+                'border-radius': '2px', 'background-color': 'green'
+            },
+        },
+        '#backFace': {
+            'align': [0.5, 0.5],
+            'origin': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'size': [250, 250],
+            'opacity': function (opacity) {
+                return opacity;
+            }
+        },
+        '.program': {
+            'align': [0.5, 0.6],
+            'origin': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'size': [140, 180],
+            '$repeat': function () {
+                return [
+                    {
+                        'style': {
+                            'background-color': 'brown'
+                        }
+                    },
+                    {
+                        'style': {
+                            'background-color': 'yellow'
+                        }
+                    },
+                    {
+                        'style': {
+                            'background-color': 'black'
+                        }
+                    },
+                    {
+                        'style': {
+                            'background-color': 'blue'
+                        }
+                    },
+                ]
+            },
+            'position-x': function ($index) {
+
+                return $index* 100;
+            }
+
         }
     },
     events: {
@@ -55,19 +102,19 @@ FamousFramework.scene('svitlana:radio', {
                 $state.set('angle', $state.get('angle') + Math.PI, {
                     duration: 1000
                 });
+                $state.set('opacity', $state.get('opacity') === 0 ? 1 : 1, {
+                    duration: 1000
+                });
             }
         }
     },
     states: {
-        numberOfClicks: 0,
-        angle: 0
+        angle: 0,
+        opacity: 0
     },
-    tree: `
-        <node id="background">
-            <node id="radioRoot">
-                <node id="radioLogo"> </node>
-                <node id="radioTitle"> </node>
-            </node>
-        </node>
-    `
+    tree: 'tree.html'
+}).config({
+    includes: [
+        'style.css'
+    ]
 });
