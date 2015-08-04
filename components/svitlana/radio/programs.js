d3a6be1d698a4d2973a47981fc5c37e090a15676
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-08-04 17:13:23 -0500

    Flipper functionality for famo.us radio #78

---
+++ components/svitlana/radio/programs.js
@@ -0,0 +1,22 @@
+var radioPrograms = [
+    {
+        'style': {
+            'background-color': 'brown'
+        }
+    },
+    {
+        'style': {
+            'background-color': 'yellow'
+        }
+    },
+    {
+        'style': {
+            'background-color': 'lightblue'
+        }
+    },
+    {
+        'style': {
+            'background-color': 'darkmagenta'
+        }
+    }
+]
--- components/svitlana/radio/radio.js
+++ components/svitlana/radio/radio.js
@@ -5,19 +5,16 @@ FamousFramework.scene('svitlana:radio', {
             'origin': [0.5, 0.5],
             'mount-point': [0.5, 0.5],
             'size': [250, 250],
-            'style': {
-                'background-color': '#F5F5F5',
-                'perspective': '10000px'
-            },
-        },
-        '#frontFace': {
-            'align': [0.5, 0.5],
-            'origin': [0.5, 0.5],
-            'mount-point': [0.5, 0.5],
-            'size': [250, 250],
+            'position-z': '1',
             'rotation-y': function (angle) {
                 return angle;
             },
+            'style': {
+                'border-radius': '2px',
+                'background-color': '#F5F5F5',
+                'perspective': '1000px'
+            },
+            'unselectable': true
         },
         '#radioTitle': {
             'content': function () {
@@ -28,6 +25,7 @@ FamousFramework.scene('svitlana:radio', {
                 'text-align': 'center',
                 'color': '#616161'
             },
+            'position-z': '2'
         },
         '#radioLogo': {
             'align': [0.5, 0.6],
@@ -38,83 +36,162 @@ FamousFramework.scene('svitlana:radio', {
                 return `<img src='https://s3-us-west-2.amazonaws.com/svet.com/radio/wasp.jpg'/>`;
             },
             'style': {
-                'border-radius': '2px'
+                'border-radius': '2px',
+                'cursor': 'pointer'
 
             },
+            'position-z': '3'
         },
         '#programsRoot': {
             'align': [0.5, 0.6],
             'origin': [0.5, 0.5],
             'mount-point': [0.5, 0.5],
-            'size': [200, 200],
+            'size': [250, 200],
             'style': {
-                'border-radius': '2px', 'background-color': 'green'
+                'border-radius': '2px',
+                'background-color': 'green',
+                'overflow': 'hidden'
+            },
+            'position-z': '-2',
+            'opacity': function (svOpacity) {
+                return svOpacity;
+            },
+
+            'rotation-y': function () {
+                return Math.PI;
             },
         },
-        '#backFace': {
+        '#slider': {
             'align': [0.5, 0.5],
             'origin': [0.5, 0.5],
             'mount-point': [0.5, 0.5],
-            'size': [250, 250],
-            'opacity': function (opacity) {
-                return opacity;
+            'size': [200, 200],
+            'style': {
+                'border-radius': '2px',
+                'background-color': 'blue',
+                'overflow': 'hidden',
+            },
+
+        },
+        '#forw': {
+            'align': [0.97, 0.5],
+            'origin': [0, 0],
+            'mount-point': [0.5, 0.5],
+            'size': [20, 20],
+            'position-z': '-2',
+            content: function () {
+                return "<div>N</div>"
+            },
+            'style': {
+                'cursor': 'pointer',
+            }
+        },
+        '#back': {
+            'align': [0.07, 0.5],
+            'origin': [0, 0],
+            'mount-point': [0.5, 0.5],
+            'size': [20, 20],
+            'position-z': '-2',
+            content: function () {
+                return "<div>P</div>"
+            },
+            'style': {
+                'cursor': 'pointer',
             }
         },
         '.program': {
-            'align': [0.5, 0.6],
+            'align': [0.5, 0.5],
             'origin': [0.5, 0.5],
             'mount-point': [0.5, 0.5],
-            'size': [140, 180],
-            '$repeat': function () {
-                return [
-                    {
-                        'style': {
-                            'background-color': 'brown'
-                        }
-                    },
-                    {
-                        'style': {
-                            'background-color': 'yellow'
-                        }
-                    },
-                    {
-                        'style': {
-                            'background-color': 'black'
-                        }
-                    },
-                    {
-                        'style': {
-                            'background-color': 'blue'
-                        }
-                    },
-                ]
-            },
-            'position-x': function ($index) {
+            'size': function (progWidth, progHeight) {
+                return [progWidth, progHeight];
 
-                return $index* 100;
-            }
+            },
+            'style': {
+                'color': 'black',
+                'text-align': 'center',
+                'font-size': '4em'
+            },
+
+            '$repeat': function (programs) {
+                return programs;
+            },
+            'content': function ($index) {
+                return '<div>' + ($index ) + '</div>';
+            },
+            'position-x': function ($index, listPosition, progWidth) {
+
+                return -($index * progWidth + listPosition + $index * 5);
+            },
+            'position-z': function ($index, activeIndex) {
+                var finalIndex = $index * 4;
+                if ($index === activeIndex) {
+                    finalIndex += 100;
+                }
+                return finalIndex;
+            },
 
         }
     },
     events: {
-        '#radioRoot': {
+        '#radioLogo,#slider': {
             'click': function ($state) {
                 $state.set('angle', $state.get('angle') + Math.PI, {
                     duration: 1000
                 });
-                $state.set('opacity', $state.get('opacity') === 0 ? 1 : 1, {
+                $state.set('svOpacity', $state.get('svOpacity') === 0 ? 1 : 0, {
                     duration: 1000
                 });
+            }
+        },
+        '#back': {
+            'click': function ($state) {
+                var listPosition = $state.get('listPosition');
+                console.log(listPosition);
+
+                if (listPosition >= 0) {
+                    return;
+                }
+                $state.set('listPosition', Math.floor($state.get('listPosition') + 145), {
+                    duration: 1000,
+                    curve: 'easeOutBounce'
+
+                });
+            }
+        },
+        '#forw': {
+            'click': function ($state) {
+                var listPosition = $state.get('listPosition');
+                var programs = $state.get('programs');
+                console.log(listPosition);
+
+                if (listPosition < (-(programs.length - 2) * 145)) {
+                    return;
+                }
+
+                 $state.set('listPosition', Math.floor($state.get('listPosition') - 145), {
+                    duration: 1000,
+                    curve: 'easeOutBounce'
+
+                });
+
             }
         }
     },
     states: {
+        progWidth: 140,
+        progHeight: 160,
+        programs: radioPrograms,
+        activeIndex: 0,
+        listPosition: 0,
+
         angle: 0,
-        opacity: 0
+        svOpacity: 0,
     },
     tree: 'tree.html'
 }).config({
     includes: [
-        'style.css'
+        'style.css',
+        'programs.js'
     ]
 });
